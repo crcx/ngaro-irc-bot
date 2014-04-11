90c90a26892f53bc0636288c9eed323f303e2100
Charles Childers <crc@forthworks.com>
2014-04-11 12:24:11 -0400

    factor out some parts of the code, make it possible to quit, and easier to configure

--- ngaro-irc.py
+++ ngaro-irc.py
@@ -1,25 +1,67 @@
+# ngaro-irc
+# This is basically an attempt to provide access to Ngaro over
+# an IRC bridge.
+#
+# Usage:
+# - configure the settings below
+# - ensure that retro.py is executable
+# - run this file
+#
+# Upon connection, to evaluate code:
+#
+# retrobot: <source>
+#
+# To quit the bot:
+#
+# retrobot: quit-now
+
 import os
 import socket
 
-irc = 'irc.freenode.net'
+server = 'irc.freenode.net'
 port = 6667
 channel = '#retro'
-sck = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sck.connect((irc, port))
-sck.send('NICK retrobot\r\n')
-sck.send('USER retrobot retrobot retrobot :retro via irc\r\n')
-sck.send('JOIN #retro' + '\r\n')
+botname = 'retrobot'
+botdesc = 'retro via irc'
+
+def sendMessage(socket, message):
+     socket.send(message + '\r\n')
+
+def joinChannel(socket, channel):
+     socket.send('JOIN ' + channel + '\r\n')
+
+def setNick(socket, nickname, descr):
+     socket.send('NICK ' + nickname + '\r\n')
+     sendMessage(socket, 'USER retrobot retrobot retrobot :' + descr)
+
+def connectToNetwork(srv, prt):
+     sck = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+     sck.connect((srv, prt))
+     return sck
+
+def sendToChannel(socket, channel, message):
+     sendMessage(socket, 'PRIVMSG ' + channel + ' :' + message)
+
+
+socket = connectToNetwork(server, port)
+setNick(socket, botname, botdesc)
+joinChannel(socket, channel)
+
 data = ''
 while True:
-     data = sck.recv(4096)
+     data = socket.recv(4096)
      if data.find('PING') != -1:
-        sck.send('PONG ' + data.split() [1] + '\r\n')
+        socket.send('PONG ' + data.split() [1] + '\r\n')
         print data
-     if data.find('retrobot:') != -1:
+     if data.find('quit-now') != -1:
+          sendMessage(socket, 'QUIT :bye!')
+          os._exit(0)
+     if data.find(botname + ':') != -1:
          fd = open('eval', 'w')
-         fd.write(data[data.find(':retrobot:')+10:] + '\r\nbye')
+         fd.write(data[data.find(':' + botname + ':')+(len(botname) + 2):] + '\r\nbye')
          fd.close()
          xx=os.popen("retro.py","r")
-         sck.send('PRIVMSG #retro :Results:\r\n')
+         sendToChannel(socket, channel, 'Results:')
          for line in xx:
-             sck.send('PRIVMSG #retro :' + line + '\r\n')
+             sendToChannel(socket, channel, line)
+             print line
